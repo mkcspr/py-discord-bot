5e9de699da6dfcf84d0373ebc2a7a749e34dd8c5
mkcspr <72995438+mkcspr@users.noreply.github.com>
2021-05-29 17:38:00 -0400

    Update pybot.py

--- pybot.py
+++ pybot.py
@@ -11,4 +11,4 @@ async def on_ready():
 	print('Use this link to invite {}:'.format(client.user.name))
 	print('https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=8'.format(client.user.id))
 
-client.run('ODQ3NjE5NTE2MjgzODc5NDM5.YLAtMA.ZeVIDBwERLzKGReF0W6K5C0uV4A')
+client.run('<TOKEN>')
